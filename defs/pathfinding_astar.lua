fac69bcce304113a6ce71d94da5ecdf5e6d24c95
julbouln <jboulnois@immateriel.fr>
2007-12-19 14:00:52 +0000

    astar trop lent

--- defs/pathfinding_astar.lua
+++ defs/pathfinding_astar.lua
@@ -1,11 +1,37 @@
+-- A* pathfinding implementation
+self.path={}
 self.current_path=1
 self.pf_open_list={}
+self.n_open_list=0
 self.pf_closed_list={}
+self.current_node=nil
 
 
 function self.pf_push_open(n)
-   self.pf_open_list[size(self.pf_open_list)]=n
-   return size(self.pf_open_list) - 1
+   local i=0
+   local e=nil
+   while i < self.n_open_list do
+      if self.pf_open_list[i]~=nil and self.pf_open_list[i].x == n.x and self.pf_open_list[i].y == n.y then
+	 e=1
+      end
+      i=i+1
+   end
+   
+   local i=0
+   while i < size(self.pf_closed_list) do
+      if self.pf_closed_list[i]~=nil and self.pf_closed_list[i].x == n.x and self.pf_closed_list[i].y == n.y then
+	 e=1
+      end
+      i=i+1
+   end
+   if e==nil then
+--      print(format("PUSH : %i,%i",n.x,n.y))
+      self.pf_open_list[self.n_open_list]=n
+      self.n_open_list=self.n_open_list + 1
+      return self.n_open_list - 1
+   else
+      return nil
+   end
 end
 
 function self.pf_push_closed(n)
@@ -38,45 +64,77 @@ function self.pf_g(n)
    return g
 end
 
+function self.pf_is_obstacle(x,y)
+   local map=self.parent.parent;   
+   local deo=map.decor.get_object_at_position(x,y)
+   if deo~=nil and deo.properties.metatype=="decoration" then
+      return 1
+   else
+      return nil
+   end
+end
+
 function self.pf_calc(p,cx,cy)
+   r=nil
+--   print(format("FROM : %i,%i",cx,cy))
    c=self.pf_push_closed(self.pf_node(p,cx,cy,10))
 
-   n1=self.pf_push_open(self.pf_node(c,cx-1,cy,10))
-   n2=self.pf_push_open(self.pf_node(c,cx+1,cy,10))
-   n3=self.pf_push_open(self.pf_node(c,cx,cy-1,10))
-   n4=self.pf_push_open(self.pf_node(c,cx,cy+1,10))
+   if self.pf_is_obstacle(cx-1,cy)==nil then
+      n1=self.pf_push_open(self.pf_node(c,cx-1,cy,10))
+   end
 
-   f=100000
+   if self.pf_is_obstacle(cx+1,cy)==nil then
+      n2=self.pf_push_open(self.pf_node(c,cx+1,cy,10))
+   end
+   if self.pf_is_obstacle(cx,cy-1)==nil then
+      n3=self.pf_push_open(self.pf_node(c,cx,cy-1,10))
+   end
+   if self.pf_is_obstacle(cx,cy+1)==nil then
+   n4=self.pf_push_open(self.pf_node(c,cx,cy+1,10))
+   end
+--   print (format("OPENLIST : %i",self.n_open_list))
+   local f=100000
    bn=nil
    local map=self.parent.parent;
 
-   i=0
-   while i~=size(self.pf_open_list) do 
+   local i=0
+
+   while i < self.n_open_list do 
       if self.pf_open_list[i] ~= nil then
 	 local ntmp=self.pf_open_list[i]
+
 	 fc=self.pf_heuristic(ntmp.x,ntmp.y) + self.pf_g(i)
+--	 print(format("%i,%i = %i",ntmp.x,ntmp.y,fc))
 	 if fc < f then
 	    f=fc
 	    bn=i
 	 end
       end
+      i=i+1
+   end   
+
+   if bn~=nil then
+      bno=self.pf_open_list[bn]
+
+
+--      print(format("PATHFINDING : %i,%i",bno.x,bno.y))
+
+      if bno.x > 0 and bno.y > 0 and bno.x < map.get_w() and bno.y < map.get_h() then
+	 if bno.x~=self.path_dx or bno.y~=self.path_dy then
+--	    print(format("PATHFINDING : %s@%s calc %i,%i",self.get_id(),self.get_type(),bno.x,bno.y))
+	    self.pf_open_list[bn]=nil	    
+	    r=self.pf_calc(bno.p,bno.x,bno.y,10)
+
+	 else
+--	    print(format("PATHFINDING : %s@%s calc destination %i,%i",self.get_id(),self.get_type(),bno.x,bno.y))
+	    self.pf_open_list[bn]=nil
+	    c=self.pf_push_closed(self.pf_node(bno.p,bno.x,bno.y,10))
+	    r=c
+	 end
+      end
    end
-
-   bno=self.pf_open_list[bn]
-   
-   if bno.x~=self.path_dx or bno.y~=self.path_dy then
---      print(format("PATHFINDING : %s@%s calc %i,%i",self.get_id(),self.get_type(),bno.x,bno.y))
-      self.pf_calc(bno.p,bno.x,bno.y,10)
-
-   else
---      print(format("PATHFINDING : %s@%s calc destination %i,%i",self.get_id(),self.get_type(),bno.x,bno.y))
-      c=self.pf_push_closed(self.pf_node(bno.p,bno.x,bno.y,10))
-   end
-   self.pf_open_list[bn]=nil
-
-
+   return r
 end
-
 
 
 function self.pathfinding()
@@ -87,10 +145,12 @@ function self.pathfinding()
    local ry=self.path_dy-cy;
 
    self.move(cx,cy)
-   local cnode=self.pf_closed_list[self.current_path]
+   local cnode=self.path[self.current_path]
+--   local cnode=self.current_node
+--   print(cnode)
 
-
-   if (rx==0 and ry==0)  or self.current_path >= size(self.pf_closed_list) then 
+   if (rx==0 and ry==0)  or cnode==nil then
+--or self.current_path >= size(self.pf_closed_list) then 
 --      print(format("PATHFINDING : %s@%s %i %i,%i destination!",self.get_id(),self.get_type(),self.current_path,cx,cy))
       
       local rt=randomize(10)+1;
@@ -137,9 +197,30 @@ function self.pathfinding()
 				   val_direction(dir) 
 			     }})
 
+--      self.current_node=self.pf_closed_list[self.current_node.p]
+--      print(self.current_node)
       self.current_path=self.current_path+1
    end
 
+
+end
+
+function self.pf_get_path(r)
+   local i=0
+   n=self.pf_closed_list[r]
+   tmpl={}
+
+   while n ~= nil do
+      tmpl[i]=n      
+      n=self.pf_closed_list[n.p]
+      i=i+1
+   end
+
+   local j=0
+   while j < size(tmpl) do
+      self.path[size(tmpl)-j-1]=tmpl[j]
+      j=j+1
+   end
 
 end
 
@@ -151,8 +232,10 @@ function self.pathfinding_start(dx,dy)
    self.pf_open_list={}
    self.pf_closed_list={}
    self.current_path=1
+   self.current_node=nil
 
-   self.pf_calc(nil,cx,cy)
---   print(format("PATHFINDING : %s@%s %i cases, %i,%i -> %i,%i",self.get_id(),self.get_type(),size(self.pf_closed_list),cx,cy,self.path_dx,self.path_dy))
+   r=self.pf_calc(nil,cx,cy)
+   self.pf_get_path(r)
+   print(format("PATHFINDING : %s@%s %i cases, %i,%i -> %i,%i",self.get_id(),self.get_type(),size(self.path),cx,cy,self.path_dx,self.path_dy))
  
 end
